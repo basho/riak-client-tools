e8a11b81203dcd68c4f3b7ecd54376314bc137b2
Luke Bakken <lbakken@basho.com>
2016-03-05 09:49:26 -0800

    add a simple chaos monkey
    add monkey count to allow bringing down up to N-1 nodes
    
    Add -z argument to disable YZ search, calculate leveldb memory, common func to get dev node count
    
    simplify getting real script path for sourcing common.bash
    
    parallelize some Riak commands

--- devrel/common.bash
+++ devrel/common.bash
@@ -60,3 +60,50 @@ function onexit
 
 trap onexit EXIT
 
+function transfers_in_progress
+{
+    local retval='in_progress'
+    local transfers_out="$(make_temp_file riak-admin-transfers)"
+
+    $riak_admin transfers > $transfers_out 2>&1
+    if grep -iqF 'Node is not running' $transfers_out
+    then
+        perr 'Riak transfers did not complete. Error!'
+        retval='error' # Return error
+    elif grep -iqF 'No transfers active' $transfers_out
+    then
+        retval='done' # No longer in progress
+    else
+        retval='in_progress' # Still in progress
+    fi
+    rm -f $transfers_out
+    echo $retval
+}
+
+function wait_for_transfers
+{
+    sleep 5
+    local transfer_status="$(transfers_in_progress)"
+    while [[ $transfer_status == 'in_progress' ]]
+    do
+        pinfo 'Transfers in progress.'
+        sleep 5
+        transfer_status="$(transfers_in_progress)"
+    done
+    pinfo "Transfer status: $transfer_status"
+
+    if [[ $transfer_status == 'error' ]]
+    then
+        errexit 'Transfers errored!'
+    fi
+}
+
+function get_dev_node_count
+{
+    local -ir dnc="$(ls -1 . | grep '^dev[0-9]\+$' | wc -l)"
+    if (( dnc == 0 ))
+    then
+        errexit "No dev nodes found in $dev_cluster_path"
+    fi
+    echo "$dnc"
+}
